65e58013736ab62564c20916bdbed219cd118653
Your Name <seawee65@live.com>
2018-07-05 18:58:14 -0400

    revised rainbow

--- src/nomarthehero/projectfun/RainbowEffect.java
+++ src/nomarthehero/projectfun/RainbowEffect.java
@@ -1,117 +1,46 @@
 package nomarthehero.projectfun;
 
-import java.util.Random;
-
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.Random;
+
 public class RainbowEffect implements Listener {
 
-	static Random rand = new Random ();
-
-	ProjectFun PF = ProjectFun.getPlugin ();
+	private ProjectFun PF = ProjectFun.getPlugin ();
 
 	@EventHandler
 	public void onChat (AsyncPlayerChatEvent event) {
 
-		if (PF.hasRainbow.contains (event.getPlayer ().getName ())) {
+		if (!PF.hasRainbow.contains (event.getPlayer ().getName ())) return;
 
-			String message = event.getMessage ();
-			String newMessage;
+		StringBuilder b = new StringBuilder ();
 
-			String[] words = message.split (" ");
+		String[] words = event.getMessage ().split (" ");
 
-			int x = rand.nextInt (15);
-			newMessage = getColorFirst (x) + words[0];
+		for (String word : words) {
 
-			for (int i = 1; i < words.length; i++) {
-				int y = rand.nextInt (15);
-				newMessage = newMessage + getColor (y) + words[i];
-
-			}
-
-			event.setMessage (newMessage);
+			b.append (randColor ());
+			b.append (word);
 
 		}
 
+		event.setMessage (b.toString ());
+
+
 	}
 
-	private String getColor (int i) {
-		switch (i) {
-			case 0:
-				return ChatColor.BLACK + " ";
-			case 1:
-				return ChatColor.DARK_BLUE + " ";
-			case 2:
-				return ChatColor.DARK_GREEN + " ";
-			case 3:
-				return ChatColor.DARK_AQUA + " ";
-			case 4:
-				return ChatColor.DARK_RED + " ";
-			case 5:
-				return ChatColor.DARK_PURPLE + " ";
-			case 6:
-				return ChatColor.GOLD + " ";
-			case 7:
-				return ChatColor.GRAY + " ";
-			case 8:
-				return ChatColor.DARK_GRAY + " ";
-			case 9:
-				return ChatColor.BLUE + " ";
-			case 10:
-				return ChatColor.GREEN + " ";
-			case 11:
-				return ChatColor.AQUA + " ";
-			case 12:
-				return ChatColor.RED + " ";
-			case 13:
-				return ChatColor.LIGHT_PURPLE + " ";
-			case 14:
-				return ChatColor.YELLOW + " ";
-			case 15:
-				return ChatColor.WHITE + " ";
-		}
-		return null;
-	}
+	private final ChatColor[] COLORS = {
+		ChatColor.BLACK, ChatColor.DARK_BLUE, ChatColor.DARK_GREEN, ChatColor.DARK_AQUA,
+		ChatColor.DARK_RED, ChatColor.DARK_PURPLE, ChatColor.GOLD, ChatColor.GRAY,
+		ChatColor.DARK_GRAY, ChatColor.BLUE, ChatColor.GREEN, ChatColor.AQUA,
+		ChatColor.RED, ChatColor.LIGHT_PURPLE, ChatColor.YELLOW, ChatColor.WHITE
+	};
 
-	private String getColorFirst (int i) {
-		switch (i) {
-			case 0:
-				return ChatColor.BLACK + "";
-			case 1:
-				return ChatColor.DARK_BLUE + "";
-			case 2:
-				return ChatColor.DARK_GREEN + "";
-			case 3:
-				return ChatColor.DARK_AQUA + "";
-			case 4:
-				return ChatColor.DARK_RED + "";
-			case 5:
-				return ChatColor.DARK_PURPLE + "";
-			case 6:
-				return ChatColor.GOLD + "";
-			case 7:
-				return ChatColor.GRAY + "";
-			case 8:
-				return ChatColor.DARK_GRAY + "";
-			case 9:
-				return ChatColor.BLUE + "";
-			case 10:
-				return ChatColor.GREEN + "";
-			case 11:
-				return ChatColor.AQUA + "";
-			case 12:
-				return ChatColor.RED + "";
-			case 13:
-				return ChatColor.LIGHT_PURPLE + "";
-			case 14:
-				return ChatColor.YELLOW + "";
-			case 15:
-				return ChatColor.WHITE + "";
-		}
-		return null;
-	}
+	private static Random rand = new Random ();
+
+	private String randColor () { return COLORS[rand.nextInt (COLORS.length)].toString (); }
 
 }
